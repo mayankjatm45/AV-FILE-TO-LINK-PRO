e9aa7b3c73b0c2e10fd25548b566645940d5f585
Mayank Jaat <jatmayankjatm45@gmail.com>
2025-08-01 09:48:54 +0530

    Update Template.py
    Signed-off-by: Mayank Jaat <jatmayankjatm45@gmail.com>

--- web/utils/Template.py
+++ web/utils/Template.py
@@ -1,5 +1,5 @@
 class avbotz_template(object):
-    NAME = "AV BOTz "
+    NAME = "Warrior Bots "
 
     DISCLAIMER = (
         "This website only provides a service to help you play your video online without downloading. "
